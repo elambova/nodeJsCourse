57f428b9c7cb5af7c1c8c9d28d196a44023a69da
Elena Lambova <elena.g.lambova@gmail.com>
2020-10-12 12:01:43 +0300

    Add param to every routes.

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const feedbackRoute = require('./feedback');
 
 const router = express.Router();
 
-module.exports = () => {
+module.exports = (params) => {
   router.get('/', (req, res) => {
     res.render('pages/index', { pageTitle: 'Welcome' });
   });
-  router.use('/speakers', speakersRoute());
-  router.use('/feedback', feedbackRoute());
+  router.use('/speakers', speakersRoute(params));
+  router.use('/feedback', feedbackRoute(params));
   return router;
 };
