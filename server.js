cc2404a7e8e89789ac82781a9f86861145225291
Elena Lambova <elena.g.lambova@gmail.com>
2020-10-12 12:00:26 +0300

    Require sevices for feedback and speakers, and create objects for them, add created objects to index route like parameter

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const path = require('path');
 
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakersService = new SpeakerService('./data/speakers.json');
+
 const routes = require('./routes');
 
 const app = express();
@@ -12,19 +18,7 @@ app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.use('/', routes());
-
-app.get('/speakers', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/speakers.html'));
-});
-
-app.get('/feedback', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/feedback.html'));
-});
-
-app.get('/Hillary_Goldwynn', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/Hillary_Goldwynn.html'));
-});
+app.use('/', routes({ feedbackService, speakersService }));
 
 app.listen(port, () => {
   console.log(`Server running in port: ${port}`);
